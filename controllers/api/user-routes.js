363cf065b6baa740f1f4ed8c519af6135e6fd591
ctnohilly <ctnohilly@gmail.com>
2022-08-07 22:58:31 -0400

    created seed data files and get routes for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,15 +1,46 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
 
-router.get('/', (req, res) => {
-    User.findAll({
-        attributes: { exclude: ['password'] }
-    })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+router.get('/', async (req, res) => {
+    try {
+        const dbUserData = await User.findAll({
+            attributes: { exclude: ['password'] }
         });
+        res.json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
+
+router.get('/:id', async (req, res) => {
+    try {
+        const dbUserData = await User.findByPk(req.params.id, {
+            attributes: { exclude: ['password'] },
+            include: [
+                {
+                    model: Post,
+                    attributes: ['id', 'title', 'created_at']
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'created_at'],
+                    include: {
+                        model: Post,
+                        attributes: ['title']
+                    }
+                }
+            ]
+        });
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id.' });
+            return;
+        }
+        res.json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
