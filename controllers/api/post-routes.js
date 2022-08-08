9d7ebde314365e1d2a7c51baff6fa52f3e8f8631
ctnohilly <ctnohilly@gmail.com>
2022-08-07 23:38:14 -0400

    created route to find post by id

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
 
+// route to get all posts
 router.get('/', async (req, res) => {
     console.log('======================');
     try {
@@ -27,5 +28,38 @@ router.get('/', async (req, res) => {
         res.status(500).json(err);
     };
 });
+
+// route to get post by id
+router.get('/:id', async (req, res) => {
+    try {
+        const dbPostData = await Post.findByPk(req.params.id, {
+            attributes: ['id', 'title', 'created_at'],
+            include: [
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                },
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
+
+
 
 module.exports = router;
