3cc7ff8bb367d9bc46a2a56623fc8a751213e911
ctnohilly <ctnohilly@gmail.com>
2022-08-07 20:25:59 -0400

    Created User model

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+// User model with a check password method
+class User extends Model {
+    // check the hashed password
+    checkPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
+
+User.init(
+    {
+        // id with auto increment and is primary key
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // username
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // email that must be unique and is validated to be an email
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        // password that must be at least 4 characters
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        hooks: {
+            // hook to run before the entry is created to hash the password
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // hook to run when entry is updated to hash the new password
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+)
