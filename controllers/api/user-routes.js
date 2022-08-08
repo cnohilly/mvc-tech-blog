4b0dcec6dc9d538f996929bf27d4c7f032798e90
ctnohilly <ctnohilly@gmail.com>
2022-08-07 23:12:04 -0400

    added post and put route for user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
 
+// route to get all users
 router.get('/', async (req, res) => {
     try {
         const dbUserData = await User.findAll({
@@ -13,6 +14,7 @@ router.get('/', async (req, res) => {
     }
 });
 
+// route to get a user by id
 router.get('/:id', async (req, res) => {
     try {
         const dbUserData = await User.findByPk(req.params.id, {
@@ -41,6 +43,49 @@ router.get('/:id', async (req, res) => {
         console.log(err);
         res.status(500).json(err);
     }
-})
+});
+
+// route to create a new user which will be used to sign up and passes the data to the session
+router.post('/', async (req, res) => {
+    // expects username: string, email: string, password: string
+    try {
+        const dbUserData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+
+            res.json(dbUserData);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
+
+router.put('/:id', async (req, res) => {
+    // expects username: string, email: string, password: string
+    // uses req.body to only update the changes that have been specified
+    try {
+        const dbUserData = User.update(req.body, {
+            individualHooks: true,
+            where: {
+                id: req.params.id
+            }
+        });
+        if (!dbUserData) {
+            res.status(404).json({ message: 'No user found with this id.' });
+            return;
+        }
+        res.json(dbUserData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    };
+});
 
 module.exports = router;
