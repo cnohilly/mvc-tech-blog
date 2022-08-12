29e2e1933123ff24dc863ff24d3396b42b674eeb
ctnohilly <ctnohilly@gmail.com>
2022-08-11 23:24:54 -0400

    wrote homepage route to display posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,27 @@
+const { Post, User } = require('../models');
+
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    const dbPostData = await Post.findAll({
+        attributes: [
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
     res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    });
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', {
+        posts,
         loggedIn: req.session.loggedIn
     });
 });
