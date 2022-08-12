afb831c17f960909fcdc0eb15eb1b34e76a217dd
ctnohilly <ctnohilly@gmail.com>
2022-08-11 23:43:12 -0400

    fixed post route and handlebars files to create populated homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,27 +3,29 @@ const { Post, User } = require('../models');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    const dbPostData = await Post.findAll({
-        attributes: [
-            'title',
-            'content',
-            'created_at'
-        ],
-        include: [
-            {
-                model: User,
-                attributes: ['username']
-            }
-        ]
-    })
-    res.render('homepage', {
-        loggedIn: req.session.loggedIn
-    });
-    const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render('homepage', {
-        posts,
-        loggedIn: req.session.loggedIn
-    });
+    try {
+        const dbPostData = await Post.findAll({
+            attributes: [
+                'title',
+                'content',
+                'created_at'
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                }
+            ]
+        });
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -3,84 +3,104 @@ const { Post } = require('../models');
 const postdata = [
   {
     title: 'Donec posuere metus vitae ipsum.',
-    user_id: 10
+    user_id: 10,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    user_id: 8
+    user_id: 8,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    user_id: 1
+    user_id: 1,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Nunc purus.',
-    user_id: 4
+    user_id: 4,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Pellentesque eget nunc.',
-    user_id: 7
+    user_id: 7,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    user_id: 4
+    user_id: 4,
+    content: "Just filled text for the testing"
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    user_id: 1
+    user_id: 1,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    user_id: 1
+    user_id: 1,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Duis ac nibh.',
-    user_id: 9
+    user_id: 9,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Curabitur at ipsum ac tellus semper interdum.',
-    user_id: 5
+    user_id: 5,
+    content: "Just filled text for the testing"
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    user_id: 3
+    user_id: 3,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    user_id: 10
+    user_id: 10,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Donec dapibus.',
-    user_id: 8
+    user_id: 8,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Nulla tellus.',
-    user_id: 3
+    user_id: 3,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-    user_id: 3
+    user_id: 3,
+    content: "Just filled text for the testing"
   },
   {
     title:
       'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-    user_id: 7
+    user_id: 7,
+    content: "Just filled text for the testing"
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    user_id: 6
+    user_id: 6,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Etiam justo.',
-    user_id: 4
+    user_id: 4,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Nulla ut erat id mauris vulputate elementum.',
-    user_id: 6
+    user_id: 6,
+    content: "Just filled text for the testing"
   },
   {
     title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    user_id: 7
+    user_id: 7,
+    content: "Just filled text for the testing"
   }
 ];
 
